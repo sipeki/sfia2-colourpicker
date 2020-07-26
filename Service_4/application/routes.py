32d26b15d055ce89a8604bb74d514671284509c9
sipeki <sipeki@kindlen.eu>
2020-07-26 11:32:34 +0000

    typo service 4 / missing routes and servervive_3 and port

--- Service_4/application/routes.py
+++ Service_4/application/routes.py
@@ -1,12 +1,12 @@
 from application import app
 import requests
 
-#   This service gets the reponses fro service 2 and 3 and combines
+#   This service gets the reponses from service 2 synonym and 3 colour and combines
 #   the result for synonym and colour.
 
-@app.route('colourpicker', methods=['GET'])
+@app.route('/colourpicker', methods=['GET'])
 def colourpicked():
     synonym = requests.get('http://service_2:5001/randomsynonym')
-    colour = requests.get('http://service_2:5001/randomcolour')
-    descriptivecolour = synonym.text + " " + colour.text
-    return descriptivecolour
+    colour = requests.get('http://service_3:5002/randomcolour')
+    response = synonym.text + " " + colour.text
+    return response
