5f3516cadb222f115029adf095d525ebc36b73ff
sipeki <sipeki@kindlen.eu>
2020-07-26 12:12:01 +0000

    Capital P for platinum service 3 colours

--- Service_3/application/routes.py
+++ Service_3/application/routes.py
@@ -8,7 +8,7 @@ def colour():
     # first list basic primary colours
     # second list tertiary pastel, metal and secondary colours
 
-    colours = [['White', 'Black', 'Red', 'Yellow', 'Blue'], ['Indigo', 'Magenta', 'Pink', 'Brown', 'Gray', 'Orange', 'Green', 'Violent', 'Purple', 'Silver', 'Gold', 'platinum']]
+    colours = [['White', 'Black', 'Red', 'Yellow', 'Blue'], ['Indigo', 'Magenta', 'Pink', 'Brown', 'Gray', 'Orange', 'Green', 'Violent', 'Purple', 'Silver', 'Gold', 'Platinum']]
 
     # Randomly selects first or second list of synonyms
     colours = choice(colours)
