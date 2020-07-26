c7183404d9aa6184cb067833b9653576f6ae08bb
sipeki <sipeki@kindlen.eu>
2020-07-26 10:48:55 +0000

    docker and requirements file for service 1 created

--- Service_1/application/routes.py
+++ Service_1/application/routes.py
@@ -6,6 +6,6 @@ import random
 @app.route('/', methods=['GET'])
 def home():
     response = requests.get('http://service_4:5003/colourpicker')
-    print(response)
+    
     colourpicked = response.text
     return render_template('index.html', colourpicked = colourpicked, title = 'Home - Colour Picker')
