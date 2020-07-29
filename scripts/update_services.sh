e25126587cf10393f0cf74ec57603e3c9ee78f52
sipeki <sipeki@kindlen.eu>
2020-07-29 11:18:42 +0000

    updated order of updates

--- scripts/update_services.sh
+++ scripts/update_services.sh
@@ -2,10 +2,11 @@
     
     # Update images in the stack
 
-    docker service update --image sipeki/service_1:latest --force sfia2_Service_1
+    docker service update --image sipeki/service_4:latest --force sfia2_Service_4
     docker service update --image sipeki/service_2:latest --force sfia2_Service_2
     docker service update --image sipeki/service_3:latest --force sfia2_Service_3
-    docker service update --image sipeki/service_4:latest --force sfia2_Service_4
+    docker service update --image sipeki/service_1:latest --force sfia2_Service_1
+    
     
     # remove orphaned containers and services
 
