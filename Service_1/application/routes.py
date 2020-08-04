0184444e3605708043161e7da0236a2dfb212a23
Simon Kindlen <sipeki@kindlen.eu>
2020-08-04 10:06:56 +0000

    Fixed database issue. Calling app = Flask(__name__) in both __init__.py and app.py only in __init__.py

--- Service_1/application/routes.py
+++ Service_1/application/routes.py
@@ -1,5 +1,5 @@
 from application import app
-from flask import Flask, render_template, request
+from flask import render_template, request
 from os import environ
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
@@ -7,29 +7,30 @@ from flask_sqlalchemy import SQLAlchemy
 import requests
 import random
 
-app = Flask(__name__)
+
 
 # Pass database security settings through OS environment
 
+ 
 app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + \
-                                        environ.get('MYSQL_USER') + \
-                                        ':' + \
-                                        environ.get('MYSQL_PASSWORD') + \
-                                        '@' + \
-                                        environ.get('MYSQL_HOST') + \
-                                        ':' + \
-                                        environ.get('MYSQL_PORT') + \
-                                        '/' + \
-                                        environ.get('MYSQL_DB_NAME')
+                                         environ.get('MYSQL_USER') + \
+                                         ':' + \
+                                         environ.get('MYSQL_PASSWORD') + \
+                                         '@' + \
+                                         environ.get('MYSQL_HOST') + \
+                                         ':' + \
+                                         environ.get('MYSQL_PORT') + \
+                                         '/' + \
+                                         environ.get('MYSQL_DB_NAME')
                                         
 
 db = SQLAlchemy(app)
 
-class ColourPicker(db.Model):
+class colours_picked(db.Model):
     ColourID = db.Column(db.Integer, primary_key=True)
     Picked = db.Column(db.String(255), nullable=False)
     User = db.Column(db.String(255), nullable=True)
@@ -51,11 +52,9 @@ def home():
     colourpicked = response.text
 
     
-    db.session.add(ColourPicker( Picked = colourpicked ))
+    db.session.add(colours_picked( Picked = colourpicked ))
     db.session.commit()
     
-    colourspicked = ColourPicker.query.order_by(ColourPicker.ColourID.desc()).limit(8).all()
+    colourspicked = colours_picked.query.order_by(colours_picked.ColourID.desc()).limit(8).all()
 
     return render_template('index.html', colourpicked = colourpicked, title = "Home - Colour Picker", colourspicked = colourspicked)
-
-    
