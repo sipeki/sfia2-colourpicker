64f63676f2de0c10c9ce5891b227b760c33444bf
Simon Kindlen <sipeki@kindlen.eu>
2020-08-05 15:36:35 +0000

    nginx Dockerfile docker compose edit start ngnix

--- scripts/start_nginx.sh
+++ scripts/start_nginx.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-docker run -d -p 80:80 --name nginx nginx
-sudo docker cp NGINX/reverse_proxy.conf nginx:/etc/nginx/conf.d/
-sudo docker exec -it nginx /etc/init.d/nginx reload
+docker build --no-cache -t sipeki/nginx ./nginx
+docker push sipeki/nginx:latest
